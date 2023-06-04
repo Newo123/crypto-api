7ff90e9f569eef8272686f417a2550db7de32ca4
Kirill <scerep529@gmail.com>
2023-06-04 16:24:27 +0600

    bugfix

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
 import { AuthResponse } from './auth.response';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/user-login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+  ) {}
 
   @ApiTags('API')
   @ApiResponse({ status: 201, type: AuthResponse })
@@ -22,5 +35,11 @@ export class AuthController {
   @Post('login')
   async login(@Body() dto: LoginUserDto): Promise<AuthResponse> {
     return this.authService.login(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('get-public-user-info')
+  async getPublicUserInfo(@Req() request) {
+    return this.userService.publicUser(request.user.email);
   }
 }
