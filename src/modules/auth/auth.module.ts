59085fc640d7a4d9f763b7ed97f25f44791a8853
Kirill <scerep529@gmail.com>
2023-06-05 09:29:47 +0600

    fix

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtStrategy } from 'src/strategy/jwt.strategy';
 import { TokenModule } from '../token/token.module';
 import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [UsersModule, TokenModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, UsersService],
 })
 export class AuthModule {}
